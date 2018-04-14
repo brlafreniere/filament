70f3e42a2cad0cb4fac801971d404e6af31bbfca
Blaine Lafreniere <brlafreniere@gmail.com>
2018-04-14 16:17:56 -0600

    Removed a bunch of stuff, simplified theme

--- footer.php
+++ footer.php
@@ -1,21 +1,8 @@
         </main>
         <footer>
-            <div class='wrapper'>
-                <section>
-                    <nav>
-                        <?php 
-                            wp_nav_menu(array(
-                                'theme_location' => 'footer-menu',
-                                'menu_id' => 'footer-menu',
-                                'container' => false,
-                            ));
-                        ?>
-                    </nav>
-                </section>
-                <section id='copyright'>
-                    &copy; <?php echo date("Y"); ?> blainefreelance.com
-                </section>
-            </div>
+            <section id='copyright'>
+                &copy; 2016 blainefreelance.com
+            </section>
         </footer>
         <?php wp_footer(); ?>
     </body>
---
+++ page.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+
+<section id='page-content'>
+    <?php the_post(); ?>
+    <?php the_content(); ?>
+</section>
+
+<?php get_footer(); ?>
