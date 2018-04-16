861f96887e64f20eab6a725b622db43955a65d74
Blaine Lafreniere <brlafreniere@gmail.com>
2018-04-15 21:31:16 -0600

    Add pagination

--- index.php
+++ index.php
@@ -6,6 +6,12 @@
     <div class='post'>
         <?php require(dirname(__FILE__) . "/_post_template.php"); ?>
     </div>
+    
 <?php } ?>
+
+<section style="display: flex; justify-content: space-between; margin: 50px 0px;">
+    <span><?php echo previous_posts_link('&larr; Newer posts'); ?></span>
+    <span><?php echo next_posts_link('Older posts &rarr;'); ?></span>
+</section>
 
 <?php get_footer(); ?>
