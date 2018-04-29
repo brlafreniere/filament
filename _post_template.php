b5ec3844f9b525ae0a9f6feabb134b0cc4316c5e
Blaine Lafreniere <brlafreniere@gmail.com>
2018-04-29 03:03:43 -0600

    Adjust font and post styles for readability

--- _post_template.php
+++ _post_template.php
@@ -3,13 +3,13 @@
         <h2>
             <a href="<?= get_the_permalink(); ?>"><?= get_the_title(); ?></a>
         </h2>
-        <div style='display: inline-block; color: #9b9b9b; font-size: 14px;'>
-            <i class="far fa-calendar-alt"></i> <?= get_the_date(); ?>
-        </div>
     </header>
     <div>
         <?php the_content(); ?>
     </div>
     <footer>
+        <div style='display: inline-block; color: #9b9b9b; font-size: 14px;'>
+            <i class="far fa-calendar-alt"></i> <?= get_the_date(); ?>
+        </div>
     </footer>
 </article>
