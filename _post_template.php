e8c079f601b3e4cc76396880fde9a1e1b66888d5
Blaine Lafreniere <brlafreniere@gmail.com>
2018-04-29 02:46:27 -0600

    Add fontawesome, calendar icon
    Plus other minor edits

--- _post_template.php
+++ _post_template.php
@@ -1,10 +1,10 @@
-<article>
+<article class='post'>
     <header>
         <h2>
             <a href="<?= get_the_permalink(); ?>"><?= get_the_title(); ?></a>
         </h2>
-        <div style='display: inline-block; color: #9b9b9b;'>
-            <?= get_the_date(); ?>
+        <div style='display: inline-block; color: #9b9b9b; font-size: 14px;'>
+            <i class="far fa-calendar-alt"></i> <?= get_the_date(); ?>
         </div>
     </header>
     <div>
--- index.php
+++ index.php
@@ -3,9 +3,7 @@
 <?php while (have_posts()) { ?>
     <?php the_post(); ?>
 
-    <div class='post'>
-        <?php require(dirname(__FILE__) . "/_post_template.php"); ?>
-    </div>
+    <?php require(dirname(__FILE__) . "/_post_template.php"); ?>
     
 <?php } ?>
 
